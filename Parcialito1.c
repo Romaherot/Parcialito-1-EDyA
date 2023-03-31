d62f137992a4135744c885fbd48981f2a651531c
Rómulo Tomás Hernández Otaño <128069878+Romaherot@users.noreply.github.com>
2023-03-31 10:45:56 -0300

    Add files via upload

---
+++ Parcialito1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int not, nottot, notnum;
+
+int main(){
+not = 0;
+nottot = 0;
+notnum = 0;
+do{
+printf("Ingrese nota de alumno\n");
+scanf("%d",&not);
+if (not==-1)
+break;
+nottot= not+nottot:
+notnum= notnum+1
+}while(not!=-1);
+printf("El promedio es %d",nottot/notnum);
+return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Parcialito-1-EDyA
